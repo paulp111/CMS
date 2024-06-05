e464ce98edf48fe3ee0444ce1fdb6a11cf16b68f
paulp111 <pichler.paul01@gmx.at>
2024-06-05 11:47:00 +0200

    page 251

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 require_once 'includes/db-connect.php';
 require_once 'includes/functions.php';
 
-
-
+// SQL-Statement für die 6 neuesten Artikel
 $sql = "SELECT a.id, a.title, a.summary, a.category_id, a.user_id,
 c.name AS category,
 CONCAT(u.forename, ' ', u.surname) AS author,
@@ -16,18 +15,36 @@ LEFT JOIN images AS i ON a.images_id = i.id
 WHERE a.published = 1
 ORDER BY a.id DESC
 LIMIT 6;";
+
 $articles = pdo_execute($pdo, $sql)->fetchAll(PDO::FETCH_ASSOC);
 
 // SQL Abfrage um alle Kategorien zu erhalten
-$sql = "SELECT id, name FROM category WHERE navigation =1;";
+$sql = "SELECT id, name FROM category WHERE navigation = 1;";
 
 $navigation = pdo_execute($pdo, $sql)->fetchAll();
 
 $title = 'IT-News';
-$description = 'All about IT and New from Software Development and Hardware';
+$description = 'All about IT and News from Software Development and Hardware';
 $section = 1;
 
 ?>
-
 <?php include './includes/header.php'; ?>
+<main class="flex flex-wrap p-8" id="content">
+    <?php foreach($articles as $article) :?>
+        <article class="w-full p-4 flex justify-between flex-col sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4">
+            <a href="article.php?id=<?= $article['id'] ?>">
+                <img src="uploads/<?= e($article['image_file'] ?? 'blank.png') ?>"
+                     alt="<?= e($article['image_alt']) ?>">
+                <h2 class="text-blue-500 text-2xl pt-3 pb-1.5"><?= e($article['title']) ?></h2>
+                <p class="text-gray-500 pb-2.5"><?= e($article['summary']) ?></p>
+            </a>
+            <p class="credit text-xs">
+                Posted in <a class="text-pink-400" href="category.php?id=<?= $article['category_id'] ?>">
+                    <?= e($article['category']) ?></a>
+                by <a class="text-pink-400" href="user.php?id=<?= $article['user_id'] ?>">
+                    <?= e($article['author']) ?></a>
+            </p>
+        </article>
+    <?php endforeach; ?>
+</main>
 <?php include './includes/footer.php'; ?>
